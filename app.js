e0f409054b97967427dfcc15330a60052656cfac
Aman Kumar Mandal <aman.mandal.71334@gmail.com>
2024-08-29 08:24:11 +0545

    Making the code modular

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const db = require('./config/mongoose-connection');
+const indexRouter = require('./routes/indexRouter');
 const ownersRouter = require('./routes/ownersRouter');
 const usersRouter = require('./routes/usersRouter');
 const productsRouter = require('./routes/productsRouter');
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+app.use('/', indexRouter)
 app.use('/owners', ownersRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
