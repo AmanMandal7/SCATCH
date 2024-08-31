037175c170b174c151aba970c5dbe6df78f4e11a
Aman Kumar Mandal <aman.mandal.71334@gmail.com>
2024-08-31 18:07:44 +0545

    Implemented add to cart feature

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const isLoggedIn = require('../middlewares/isLoggedIn');
 const productModel = require('../models/product-model');
+const userModel = require('../models/user-model');
 const router = express.Router();
 
 router.get('/', (req, res)=> {
     let error = req.flash("error");
-    res.render('index', {error})
+    res.render('index', {error, loggedin: false})
 });
 
 router.get('/shop', isLoggedIn, async (req, res)=> {
-    let products =await productModel.find()
-    res.render("shop", {products});
+    let products =await productModel.find();
+    let success = req.flash('success')
+    res.render("shop", {products, success});
+});
+
+router.get('/addtocart/:productid', isLoggedIn, async (req, res)=> {
+    let user = await userModel.findOne({email: req.user.email});
+    user.cart.push(req.params.productid);
+    await user.save();
+    req.flash("success", "Added to cart");
+    res.redirect('/shop')
 })
 
 module.exports = router;
